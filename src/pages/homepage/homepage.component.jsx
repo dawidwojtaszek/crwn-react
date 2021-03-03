046c1d976a175ab1f6b2f94a2299a077fc23d550
unknown <dawidw22@gmail.com>
2021-03-03 11:02:11 +0100

    broken homepage into smaller components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.style.scss'
+import DirectoryMenu from '../../components/directory/directory.component';
+const Homepage = () => (
+
+    <DirectoryMenu />
+
+)
+
+export default Homepage
